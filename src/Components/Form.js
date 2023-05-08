30579f164d9c623c8d055325d0ca89e7aff8f618
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-05-08 12:33:10 +0200

    Validators and data POST to api

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,74 +5,92 @@ import Axios from 'axios';
 export default function Form() {
   const URL = 'https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes';
   const [dish, setDish] = useState({
-    "name": "",
-    "preparation_time": "",
-    "type": "",
-    "id": crypto.randomUUID(),
+    id: 0,
+    name: "",
+    preparation_time: "",
+    type: "",
+    no_of_slices: Number,
+    diameter: Number,
+    spiciness_scale: Number,
+    slices_of_bread: Number,
   });
 
   // showing and hiding divs depended on which option is selected
   const [currentDiv, setCurrentDiv] = useState('');
-  const newDish = {...dish};
-
   function handleDish(event){
+    const newDish = {...dish};
     newDish[event.target.id] = event.target.value;
     setDish(newDish);
     console.log(newDish);
     // showing and hiding divs9
-    setCurrentDiv(newDish.type)
+    setCurrentDiv(newDish.type);
   }
-
   const optionsType = [
     {
       value: '',
       label: "Select..."
     },
     {
-      value: "Pizza",
+      value: "pizza",
       label: "Pizza"
     },
     {
-      value: "Soup",
+      value: "soup",
       label: "Soup"
     },
     {
-      value: "Sandwich",
+      value: "sandwich",
       label: "Sandwich"
     }
   ];
 
   // validation
-  const errors = {
-    dishName: 'Please type a dish name',
-    preparationTime: 'Please select a preparation time',
-    type: 'Please choose a type of your dish',
-    pizza: {
-      numberOfSlices: 'Please type a number of slices',
-      diameter: 'Please type a diameter',
-    },
-    soup: {
-      spicinesScale: 'Please type a spicines scale'
-    },
-    sandwich: {
-      slicesOfBread: 'Please type how many slices of bread you want'
-    },
-  }
-  const [responseStatusError, setResponseStatusError] = useState('');
   const [focused, setFocused] = useState(false);
   const handleFocus = (event) => {
     setFocused(true);
   }
-
+  const errors = {
+    dishName: 'Please type a dish name. Dish name must be at least 3 characters.',
+    preparationTime: 'Please select a preparation time. Format HH:MM:SS.',
+    type: 'Please choose a type of your dish',
+    pizza: {
+      numberOfSlices: 'Please type a number of slices. Integers only. Type between 1-16 slices.',
+      diameter: 'Please type a diameter',
+    },
+    soup: {
+      spicinesScale: 'Please type a spicines scale. Integers only. Type between 1-10.'
+    },
+    sandwich: {
+      slicesOfBread: 'Please type how many slices of bread you want. Integers only. Type between 1-10 slices.'
+    },
+  }
   // sending form
+  const [responseStatusError, setResponseStatusError] = useState('');
   function submitForm(event){
+    // removing unnecessary values from object
+    let dishToPost = {
+      id: dish.id,
+      name: dish.name,
+      preparation_time: dish.preparation_time,
+      type: dish.type,
+    }
+    if(currentDiv === 'pizza'){
+      dishToPost.no_of_slices = dish.no_of_slices
+      dishToPost.diameter = dish.diameter
+    } else if(currentDiv === 'soup'){
+      dishToPost.spiciness_scale = dish.spiciness_scale
+    } else if(currentDiv === 'sandwich'){
+      dishToPost.slices_of_bread = dish.slices_of_bread
+    }
+    // axios post
     event.preventDefault();
-    Axios.post(URL, {
-      dish
-    }).then(response => {
+    Axios.post(URL, 
+      dishToPost
+      ).then(response => {
+      setResponseStatusError('Data submitted!')
       console.log(response);
     }).catch(error => {
-      if(error.response.status >= 400 || error.response.code <= 500){
+      if(error.response.status >= 400 || error.response.code <= 499){
         setResponseStatusError('Client error response! Try again later.')
       } else if(error.response.status >= 500 || error.response.code <= 599){
         return setResponseStatusError('Wrong server response! Try again later or contact our support.')
@@ -89,7 +107,7 @@ export default function Form() {
         <div>
           <label>Dish name</label>
           <div>
-            <input type="text" onChange={(event) => handleDish(event)} id="name" value={dish.name} onBlur={handleFocus} focused={focused.toString()} required/>
+            <input type="text" onChange={(event) => handleDish(event)} id="name" value={dish.name} onBlur={handleFocus} focused={focused.toString()} minLength={3} required/>
             <span>{errors.dishName}</span>
           </div>
         </div>
@@ -118,12 +136,12 @@ export default function Form() {
 
         <div>
           {
-            currentDiv === 'Pizza' && (
+            currentDiv === 'pizza' && (
               <div>
                 <div>
                   <label>Number of slices</label>
                   <div>
-                    <input type="number" onChange={(event) => handleDish(event)} id="no_of_slices" value={dish.no_of_slices || 0} onBlur={handleFocus} focused={focused.toString()} required/>
+                    <input type="number" onChange={(event) => handleDish(event)} id="no_of_slices" value={dish.no_of_slices} onBlur={handleFocus} focused={focused.toString()} min="1" max="16" required/>
                     <span>{errors.pizza.numberOfSlices}</span>
                   </div>
                 </div>
@@ -131,7 +149,7 @@ export default function Form() {
                 <div>
                   <label>Diameter</label>
                   <div>
-                    <input type="number" step="0.01" onChange={(event) => handleDish(event)} id="diameter" value={dish.diameter || 0} onBlur={handleFocus} focused={focused.toString()} required/>
+                    <input type="number" step="0.01" onChange={(event) => handleDish(event)} id="diameter" value={dish.diameter} onBlur={handleFocus} focused={focused.toString()} required/>
                     <span>{errors.pizza.diameter}</span>
                   </div>
                 </div>
@@ -140,22 +158,22 @@ export default function Form() {
             )
           }
           {
-            currentDiv === 'Soup' && (
+            currentDiv === 'soup' && (
               <div>
                 <label>Spiciness scale</label>
                 <div>
-                  <input type="number" onChange={(event) => handleDish(event)} id="spiciness_scale" value={dish.spiciness_scale || 0} onBlur={handleFocus} focused={focused.toString()} required />
+                  <input type="number" onChange={(event) => handleDish(event)} id="spiciness_scale" value={dish.spiciness_scale} onBlur={handleFocus} focused={focused.toString()} min="1" max="10" required />
                   <span>{errors.soup.spicinesScale}</span>
                 </div>
               </div>
             )
           }
           {
-            currentDiv === 'Sandwich' && (
+            currentDiv === 'sandwich' && (
               <div>
                 <label>Slices of bread</label>
                 <div>
-                  <input type="number" onChange={(event) => handleDish(event)} id="slices_of_bread" value={dish.slices_of_bread || 0} onBlur={handleFocus} focused={focused.toString()} required/>
+                  <input type="number" onChange={(event) => handleDish(event)} id="slices_of_bread" value={dish.slices_of_bread} onBlur={handleFocus} focused={focused.toString()} min="1" max="10" required/>
                   <span>{errors.sandwich.slicesOfBread}</span>
                 </div>
               </div>
